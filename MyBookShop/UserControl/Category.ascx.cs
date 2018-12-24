cd337aa964a39902a20151096f37f84723a903a9
fcxl9876 <sadj9876@qq.com>
2018-12-24 09:17:41 +0800

    update usercontrol

--- MyBookShop/UserControl/Category.ascx.cs
+++ MyBookShop/UserControl/Category.ascx.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class Category : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        MyBookShopDataContext db = new MyBookShopDataContext();
+        //查询分类号、分类名、该分类包含的商品
+        var results = from c in db.Category
+                      join b in db.Book on c.CategoryId equals b.CategoryId
+                      into book
+                      select new
+                      {
+                          c.CategoryId,
+                          c.Name,
+                          BookCount = book.Count()
+                      };
+        gvCategory.DataSource = results;
+        gvCategory.DataBind();
     }
 }
--- MyBookShop/UserControl/NewProduct.ascx.cs
+++ MyBookShop/UserControl/NewProduct.ascx.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class UserControl_NewProduct : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        MyBookShopDataContext db = new MyBookShopDataContext();
+        var books = (from b in db.Book
+                        orderby b.BookId descending
+                        select b).Take(7);
+        gvProduct.DataSource = books;
+        gvProduct.DataBind();
     }
 }
