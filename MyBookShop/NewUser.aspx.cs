352272f9bc83f5b7974f2a3196984665ed5b5009
fcxl9876 <sadj9876@qq.com>
2018-12-28 23:13:17 +0800

    final code

--- MyBookShop/NewUser.aspx.cs
+++ MyBookShop/NewUser.aspx.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Web.Security;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class NewUser : System.Web.UI.Page
 {
--- MyBookShop/OrderList.aspx.cs
+++ MyBookShop/OrderList.aspx.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class OrderList : System.Web.UI.Page
